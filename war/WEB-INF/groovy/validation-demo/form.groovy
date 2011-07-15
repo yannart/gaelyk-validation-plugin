561824015efa6433d90e530df1fd3775e3367c40
Yann Nicolas <yannart@gmail.com>
2011-07-15 00:29:02 -0500

    Now uses in the example a cached instance of the JSON constraint descriptor generator insted of the non cached.

--- war/WEB-INF/groovy/validation-demo/form.groovy
+++ war/WEB-INF/groovy/validation-demo/form.groovy
@@ -2,7 +2,9 @@ import com.yannart.entity.User
 import com.yannart.validation.json.JSR303ToJSONConstraintDescriptor
 
 //Client side validation
-def clientValidationRules = JSR303ToJSONConstraintDescriptor.getInstance().render(User.class)
+def clientValidationRules = JSR303ToJSONConstraintDescriptor.getCachedInstance().render(User.class)
+//Use JSR303ToJSONConstraintDescriptor.getInstance().render(User.class) to get an instance that do not cache the rendered JSON.
+//Or call JSR303ToJSONConstraintDescriptor.getCachedInstance().render(User.class) to reset the cache for that bean
 
 log.info "Validation rules: ${clientValidationRules}"
 
